439983850264d4e4afeb2c0cd6bd3999f3d24c82
Megha Rastogi <compchap.megha@gmail.com>
2017-06-03 18:25:03 -0700

    Create PlayerComparator.java

---
+++ Comparator/PlayerComparator.java
@@ -0,0 +1,41 @@
+class Checker implements Comparator<Player>{
+    
+    public int compare(Player first, Player second){
+        if(first.score != second.score){
+            return second.score - first.score;   
+        }
+        return first.name.compareTo(second.name);
+       
+    }
+}
+
+class Player{
+    String name;
+    int score;
+    
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class PlayerComparator {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
